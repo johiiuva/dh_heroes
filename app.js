dca71994cd2d0fe92846f26f7f2132e2623cf3c1
johiiuva <johiiuva@gmail.com>
2023-10-03 11:26:08 -0300

    creando pagina de heroes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 3030;
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','index.html')))
+app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, 'views','babbage.html')))
+app.get('/banners-lee', (req,res) => res.sendFile(path.join(__dirname, 'views','banners-lee.html')))
+app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, 'views','babbage.html')))
+app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname, 'views','hamilton.html')))
+app.get('/hopper', (req,res) => res.sendFile(path.join(__dirname, 'views','hopper.html')))
+
+app.get('/lovelace', (req,res) => res.sendFile(path.join(__dirname, 'views','lovelace.html')))
+app.get('/turing', (req,res) => res.sendFile(path.join(__dirname, 'views','turing.html')))
+
+
+
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
